bede0914d1b5500a8e5dc79cae81a3b47d4b7fa6
Avinash175 <avinashmagar15@gmail.com>
2026-06-04 13:37:55 +0530

    refactor: simplify Ionicons import and replace punch card with a container View in AttendanceScreen

--- src/icons/Ionicons.ts
+++ src/icons/Ionicons.ts
@@ -1,10 +1,3 @@
-import createIconSet from '@expo/vector-icons/build/vendor/react-native-vector-icons/lib/create-icon-set';
-import glyphMap from '@expo/vector-icons/build/vendor/react-native-vector-icons/glyphmaps/Ionicons.json';
-
-/**
- * Ionicons via @expo/vector-icons, loaded from native font file.
- * Android expects assets/fonts/ionicons.ttf (see postinstall script).
- */
-const Ionicons = createIconSet(glyphMap, 'Ionicons', 'ionicons.ttf');
+import { Ionicons } from '@expo/vector-icons';
 
 export default Ionicons;
